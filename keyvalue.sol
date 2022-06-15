0a718bffb1b08156d4061f43b4d1c458dc67eb64
nuwrldnf8r <nuwrldnf8r@gmail.com>
2022-06-15 16:39:35 +0200

    Update keyvalue.sol

--- keyvalue.sol
+++ keyvalue.sol
@@ -6,7 +6,14 @@ contract KeyValue {
     mapping (bytes32 => string) private _keyval;
 
     /**@dev
-    * Sets value (data) with key
+    * Sets value (data) with bytes32 key
+    **/
+    function set(bytes32 key, string memory value) public{
+         _keyval[getKey(msg.sender,key)] = value;
+    }
+
+    /**@dev
+    * Sets value (data) with string key
     **/
     function set(string memory key, string memory value) public{
         bytes memory b = bytes(key);
@@ -30,6 +37,10 @@ contract KeyValue {
         return keccak256(abi.encode(owner,key));
     }
 
+    function getKey(address owner, bytes32 key) public pure returns (bytes32){
+        return keccak256(abi.encode(owner,key));
+    }
+
     /**@dev
     * returns data for a given key. Will only return correctly for the owner of the data
     **/
@@ -48,7 +59,16 @@ contract KeyValue {
     * returns data for a bytes32 key - returned by getKey(address owner, string memory key)
     **/
     function get(bytes32 key) public view returns (string memory){
+        string memory data = _keyval[getKey(msg.sender,key)];
+        if(bytes(data).length>0) return data;
         return _keyval[key];
+    }
+
+    /**@dev
+    * returns data for a given key and the address of the owner of the data
+    **/
+    function get(address owner, bytes32 key) public view returns (string memory){
+        return _keyval[getKey(owner,key)];
     }
 
 }
